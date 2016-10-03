24d39f8e9be85b9f67b996e81b42030654874a45
Silvia <silvia.quintanar.robles@gmail.com>
2016-10-03 12:34:09 +0200

    He incluido mi localidad de nacimiento

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,5 +3,6 @@ public class HolaMundo{
 	public static void main (String args[]){
 		System.out.println("Silvia Quintanar Robles");
 		System.out.println("Tengo 22 años");
+		System.out.println("Mi localidad de nacimiento es Campo de Criptana");
 	}
 }
